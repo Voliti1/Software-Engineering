a970f464b3091b528505209628a8513914ab21a3
Voliti1 <sjwnick2010@gmail.com>
2026-05-15 12:17:49 +0900

    Update contact_table.sql

--- contact_table.sql
+++ contact_table.sql
@@ -10,5 +10,7 @@ create table contact(
 
 desc contact;
 
-insert into contact values(null, "박찬호", "010-1234-1234", "ChanhoPark@gmail.com", "서울특별시 중구");
+insert into contact values(null, "박찬호", "010-1234-1234", "chanhoPark@gmail.com", "서울특별시 중구");
 insert into contact values(null, "박지성", "010-1234-4321", "JisungPark@gmail.com", "경기도 수원시");
+
+select * from contact;
